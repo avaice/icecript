6bbb095c6d41aa9ab74b604ea1ea1fb2f5c83681
avaice <dev@cho-ice.xyz>
2023-09-25 18:21:52 +0900

    DOM操作系関数の無効化機能

--- src/webEmbed.ts
+++ src/webEmbed.ts
@@ -7,13 +7,20 @@ import { writeHtml } from './functions/libDom/writeHtml'
 import { readTextForBrowser } from './functions/readText/browser'
 import { exec } from './tools'
 
-// Browser実装依存のfunctions
-;(functions as any)['input'] = inputForBrowser
-;(functions as any)['writeHtml'] = writeHtml
-;(functions as any)['movePage'] = movePage
-;(functions as any)['setTitle'] = setTitle
-;(functions as any)['listenWebEvent'] = listenWebEvent
-;(functions as any)['readText'] = readTextForBrowser
+const setBrowserFunctions = (noDomFunctions?: boolean) => {
+  // Browser実装依存のfunctions
+  ;(functions as any)['input'] = inputForBrowser
+  ;(functions as any)['readText'] = readTextForBrowser
+  if (!noDomFunctions) {
+    ;(functions as any)['writeHtml'] = writeHtml
+    ;(functions as any)['movePage'] = movePage
+    ;(functions as any)['setTitle'] = setTitle
+    ;(functions as any)['listenWebEvent'] = listenWebEvent
+  }
+}
 
 //exec
-;(window as any).icecript = exec
+;(window as any).icecript = (src: string, flag?: any, noDomFunctions?: boolean) => {
+  setBrowserFunctions(noDomFunctions)
+  exec(src, flag)
+}
